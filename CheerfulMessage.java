086b37f85da9b6f1596684ff959ccf32acf576a6
Sanika Sree A K <145793335+sanika-ak@users.noreply.github.com>
2025-01-30 22:37:15 +0530

    CheerfulMessage.java
    Imagine you need to repeat a cheerful message. Write a program that uses a for 
    loop to print "ALL IS WELL" exactly twenty times. How will you set up your loop to 
    ensure this message appears the right number of times?

---
+++ CheerfulMessage.java
@@ -0,0 +1,8 @@
+public class CheerfulMessage {
+    public static void main(String[] args) {
+        // Loop runs from 1 to 20 as '<=' operator is used it prints 20 time including the number range
+        for (int i = 1; i <= 20; i++) {
+            System.out.println("ALL IS WELL");
+        }
+    }
+}
